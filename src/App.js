326f74fcfb073d6e7cf64c2c167bae73e8eca116
emmakujala <emmakujala@hotmail.com>
2023-10-13 15:19:43 +0200

    ShowCocktails

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ShowCoctails from './ShowCocktails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +16,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="wrapper">
+
+        <ShowCoctails></ShowCoctails>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
