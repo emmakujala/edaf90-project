c7182413b49112a8d210262cde7f5b2f89c190cd
Thilda Holmner <thilda.holmner.8080@student.lu.se>
2023-10-13 14:37:20 +0200

    app file created ++ header and footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="container py-4">
+        <header className="pb-3 mb-4 border-bottom">
+          <span className="fs-4">DIN MAMMAS COCKTAILBAR</span>
+        </header>
 
+        <footer className="pt-3 mt-4 text-muted border-top">
+        EDAF90 - webprogrammering
+      </footer>
+        </div>
+    )
+}
 export default App;
