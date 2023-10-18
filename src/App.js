451051e3f378a95d9bacbfec39fbaa444a9a36ed
Märta Holmquist <marta.holmquist.1038@student.lu.se>
2023-10-18 11:04:06 +0200

    Fix navbar, remove headers container due to duplicate functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import ShowCocktails from './ShowCocktails'
 import 'bootstrap/dist/css/bootstrap.css';
-import { CocktailProvider } from './CocktailProvider';
-import ShowForm from './SearchBar';
 import Container from 'react-bootstrap/Container'
-import Header from './Header'
 import Image from 'react-bootstrap/Image'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import SearchBar from './SearchBar';
 
@@ -16,29 +12,15 @@ function App() {
     <div className="App">
       <Container>
         <header>
-          <Image src="bananacocktail (kopia).jpeg" fluid />
-          <Header>
-          </Header>
+          <Image src="/bananacocktail (kopia).jpeg" fluid />
+          <NavBar />
+          <Routes>
+            <Route path="nav/list" element={<ShowCocktails />} />
+            <Route path="nav/search" element={<SearchBar />} />
+          </Routes>
         </header>
       </Container>
-      <body>
-        <NavBar />
-        <Routes>
-          <Route path="nav/list" element={<ShowCocktails />} />
-          <Route path="nav/search" element={<SearchBar />} />
-          {/* Other routes */}
-        </Routes>
-        {/* <Outlet></Outlet>
-          <NavBar></NavBar>
-          
-            <Container> */}
-        {/* <ShowForm></ShowForm> */}
-        {/* </Container> */}
-        {/* <ShowCocktails></ShowCocktails> */}
 
-
-
-      </body>
       <footer className="pt-3 mt-4 text-muted border-top">
         EDAF90 - webprogrammering
       </footer>
