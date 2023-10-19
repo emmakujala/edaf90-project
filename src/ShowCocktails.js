c88611cc6502e3aa175a8ef47661b510a1d67284
Märta Holmquist <marta.holmquist.1038@student.lu.se>
2023-10-19 10:09:03 +0200

    Fix cache

--- src/ShowCocktails.js
+++ src/ShowCocktails.js
@@ -1,55 +1,17 @@
-import { useEffect, useState } from "react";
 import { Outlet, useNavigate, Route, Routes } from "react-router-dom";
 import { useCocktailContext } from './CocktailProvider';
 import CocktailDetails from "./CocktailDetails";
 
 export default function ShowCocktails() {
-    const [cocktails, setCocktails] = useState([]);
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    const url = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
     const { state, dispatch } = useCocktailContext();
     const navigate = useNavigate();
-
-
-    useEffect(() => {
-
-        async function getCocktails() {
-            try {
-                let allCocktails = [];
-
-                for (let letter of alphabet) {
-                    const response = await fetch(url + letter);
-                    const result = await response.json();
-
-                    if (result.drinks) {
-                        allCocktails = [...allCocktails, ...result.drinks]
-                    }
-                }
-                
-                setCocktails(allCocktails);
-                
-                
-                 
-            } catch (error) {
-                console.error(error);
-            }
-            
-        }
-        getCocktails();
-       
-    }, [])
 
     
     const show = ((newCocktail) => {
        dispatch({ type: 'SET_SELECTED_COCKTAIL', payload: newCocktail })
-        
         navigate('/list/details');
 
-
     })
-
-    
-     
 
     return (
 
@@ -57,8 +19,8 @@ export default function ShowCocktails() {
           
             <select value={state.selectedCocktail} onChange={(e) => show(e.target.value)} className="btn btn-info dropdown-toggle" position="absolute">
             <option value=''>Choose a drink</option>
-            {cocktails.map((cocktail, index) => (
-                <option value={cocktail.strDrink} key={index}>{cocktail.strDrink}</option>  
+            {state.cocktailNames.map((cocktail, index) => (
+                <option value={cocktail} key={index}>{cocktail}</option>  
             )
             
             )}
